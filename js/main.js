ecbf0ef05087a5730363d95ac25878be8d2abe61
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2025-01-14 21:29:55 +0000

    Added an event listener to update img src. #1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
+'use strict';
 const $imgInput = document.querySelector('#img-url');
 const $placeholderImg = document.querySelector('.placeholder-image');
-
 if (!$imgInput) throw new Error('$imgInput query failed.');
 if (!$placeholderImg) throw new Error('$placeholderImg query failed.');
-
-$imgInput.addEventListener('input', (event: Event) => {
-  const $eventTarget = event.target as HTMLInputElement;
+$imgInput.addEventListener('input', (event) => {
+  const $eventTarget = event.target;
   const $imgSrc = $eventTarget.value;
-
   $placeholderImg.setAttribute('src', $imgSrc);
 });
